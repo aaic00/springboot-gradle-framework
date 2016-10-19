0183c0aef8f53b21bcd3972c749f2107128680ce
aAic <996202@qq.com>
2016-10-20 01:35:59 +0800

    druid

---
+++ src/main/java/com/xjs/Application.java
@@ -0,0 +1,13 @@
+package com.xjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class Application {
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
--- src/main/java/com/xjs/hello/SampleController.java
+++ src/main/java/com/xjs/hello/SampleController.java
@@ -1,18 +1,21 @@
-package hello;
+package com.xjs.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
   @RequestMapping("/")
-  @ResponseBody
   String home() {
+    return "Hello World!";
+  }
+
+  @RequestMapping("/hello")
+  String home1() {
     return "Hello World!";
   }
 
